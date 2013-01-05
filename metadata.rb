7323e871e3428ca0a1ab9d6cd226fdeabf3e6af4
David P Kleinschmidt <david@kleinschmidt.name>
2013-01-05 13:39:02 -0500

    docs

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ maintainer       'David P. Kleinschmidt'
 maintainer_email 'david@kleinschmidt.name'
 license          'MIT'
 description      'Clean up your cookbooks with ingredients'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          '0.1.0'
