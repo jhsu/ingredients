b01fba8a0ec18c4ce391357525c3fb3b83c08f48
Joseph Hsu <jhsu.x1@gmail.com>
2013-01-08 01:15:50 -0500

    Fix metadata to correctly look for README.md

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ maintainer       'David P. Kleinschmidt'
 maintainer_email 'david@kleinschmidt.name'
 license          'MIT'
 description      'Clean up your cookbooks with ingredients'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
